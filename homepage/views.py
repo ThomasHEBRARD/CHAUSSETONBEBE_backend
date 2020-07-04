081fddc937dde6f8e49e0758488ecbedfd94abd6
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-07-04 22:40:56 +0200

    setting up basic view

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.viewsets import ViewSet
+from .serializers import HomepageSerializer
+from .models import TestModel
 
 
-def index(request):
-    return HttpResponse('Hello world')
+def HomepageView(ViewSet):
+    return HttpResponse("Hello world")
+
+    
+    
