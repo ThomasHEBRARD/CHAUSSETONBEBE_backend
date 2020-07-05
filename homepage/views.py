5f5efdc1fa8715b0d17b34c4168c5473f3116e50
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-07-05 17:43:16 +0200

    quick fix

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .serializers import HomePageSerializer
 from .models import HomePageModel
 
@@ -8,6 +9,9 @@ from .models import HomePageModel
 class HomePageViewSet(generics.ListCreateAPIView):
     queryset = HomePageModel.objects.all()
     serializer_class = HomePageSerializer
+
+    def get_queryset(self):
+        return self.queryset
 
 class HomePageViewSetDetailed(generics.RetrieveUpdateDestroyAPIView):
     queryset = HomePageModel.objects.all()
