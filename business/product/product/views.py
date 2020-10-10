00bc67ead753b4557a278a04ec6445e537dbacd2
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-10-10 11:42:13 +0200

    compete image field upload and fetch

--- business/product/product/views.py
+++ business/product/product/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
 from business.product.product.serializers import ProductSerializer
 from business.product.product.models import Product
 
@@ -9,6 +10,7 @@ from business.product.product.models import Product
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
         return self.queryset.order_by("id")
